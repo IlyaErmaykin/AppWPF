25ea6cb1fe08de82314cb588f64291bd68752ff4
Ермайкин Илья <ilya_ermaykin@mail.ru>
2018-10-02 16:31:15 +0400

    #8 AppForDependencyProperty - Промежутосный коммит;  - Создание свойства зивисимости;

--- AppForDependencyProperty/ShowNumberControl.xaml.cs
+++ AppForDependencyProperty/ShowNumberControl.xaml.cs
@@ -17,17 +17,64 @@ namespace AppForDependencyProperty
 {
     /// <summary>
     /// Логика взаимодействия для ShowNumberControl.xaml
+    /// Logic of interaction for ShowNumberControl.xaml
     /// </summary>
     public partial class ShowNumberControl : UserControl
     {
+        /// <summary>
+        /// Default contructor
+        /// </summary>
         public ShowNumberControl()
         {
             InitializeComponent();
         }
 
+        // Свойство зависимости (CLR)
+        public int MyProperty
+        {
+            get { return (int)GetValue(CurrentNumberProperty); }
+            set { SetValue(CurrentNumberProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for MyProperty.  This enables animation, styling, binding, etc...
+        // Обратите внимание на второй параметр конструктора UIPropertyMetadata
+        public static readonly DependencyProperty CurrentNumberProperty =
+            DependencyProperty.Register("CurrentNumber", typeof(int),
+                typeof(ShowNumberControl),
+                new UIPropertyMetadata(100,
+                    new PropertyChsangedCallack(CurrentNumberChanged)),
+                new ValidateValueCallback(ValidateCurrentNumber)
+            );
+
+        /// <summary>
+        /// Validate current number function
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static bool ValidateCurrentNumber(object value)
+        {
+            // Очень простое бизнес-правило: значение должно находиться в диапазоне между 0 и 500
+            if (Convert.ToInt32(value) >= 0 && Convert.ToInt32(value) <= 500)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+
         // Обычное свойство .NET
+        /// <summary>
+        /// Currentr number field
+        /// </summary>
         private int currNumber = 0;
 
+        /// <summary>
+        /// Currentr number property
+        /// </summary>
         public int CurrentrNumber
         {
             get
