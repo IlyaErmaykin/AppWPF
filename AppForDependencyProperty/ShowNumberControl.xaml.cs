7e734bb4707d3ac62d4b45790efe7473a442fd7e
Ермайкин Илья <ilya_ermaykin@mail.ru>
2018-10-02 15:22:48 +0400

    #7 Создание AppForDependencyProperty;  - Начальный функционал;

---
+++ AppForDependencyProperty/ShowNumberControl.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AppForDependencyProperty
+{
+    /// <summary>
+    /// Логика взаимодействия для ShowNumberControl.xaml
+    /// </summary>
+    public partial class ShowNumberControl : UserControl
+    {
+        public ShowNumberControl()
+        {
+            InitializeComponent();
+        }
+
+        // Обычное свойство .NET
+        private int currNumber = 0;
+
+        public int CurrentrNumber
+        {
+            get
+            {
+                return this.currNumber;
+            }
+
+            set
+            {
+                this.currNumber = value;
+
+                numberDisplay.Content = CurrentrNumber.ToString();
+            }
+
+        }
+    }
+}
--- ConvertBmpToPng/ImageToBitmapConverter.cs
+++ ConvertBmpToPng/ImageToBitmapConverter.cs
@@ -18,18 +18,68 @@ namespace ConvertBmpToPng
 {
     internal class ImageToBitmapConverter : IValueConverter
     {
+        public BitmapImage ConvertByteArrayToImage(byte[] imageByteArray)
+        {
+            BitmapImage img = new BitmapImage();
+
+            using (MemoryStream memoryStream = new MemoryStream(imageByteArray))
+            {
+                return null;
+            }
+        }
+
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            var _path = (string)value;
-
-            using (var myBitmap = new Bitmap(_path))
+            using (MemoryStream memoryStrem = new MemoryStream())
             {
-                myBitmap.MakeTransparent(System.Drawing.Color.White);
+                var _path = (string)value;
 
-                myBitmap.Save(_path, ImageFormat.Png);
+                using (var myBitmap = new Bitmap(_path))
+                {
+                    myBitmap.MakeTransparent(System.Drawing.Color.White);
+
+                    myBitmap.Save(memoryStrem, System.Drawing.Imaging.ImageFormat.Png);
+
+                    //MesageBox.Show(myBitmap);
+                    return myBitmap;
+
+                }
+
+                //myBitmap.Save(_path, ImageFormat.Png);
             }
 
-            return _path;
+            //return _path;
+
+            //using (var myBitmap = new Bitmap(_path))
+            //{
+            //    myBitmap.MakeTransparent(Color.White);
+
+            //    //Переделать
+            //    //_path = _path.Replace(".bmp", ".png");
+
+            //    //myBitmap.Save(_path, ImageFormat.Png);
+            //}
+
+            //byte[] file = File.ReadAllBytes(_path);
+
+            //using (MemoryStream byteStream = new MemoryStream(file))
+            //{
+            //    using (BinaryReader reader = new BinaryReader(byteStream))
+            //    {
+            //        for (int i = 0; i < file.Length; i++)
+            //        {
+            //            byte result;
+            //            using (Bitmap myBitmap = new Bitmap(reader.ToString()))
+            //            {
+            //                myBitmap.MakeTransparent(Color.White);
+            //                result = reader.ReadByte();
+            //            }
+
+            //            //result = reader.ReadByte();
+            //            MessageBox.Show(result.ToString());
+            //        }
+            //    }
+            //}
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
@@ -39,7 +89,7 @@ namespace ConvertBmpToPng
 
         private object GetBackgrounColor()
         {
-            
+
 
 
             Bitmap bitmap = new Bitmap(@"C:\Projects\Spotweld_New\Spotweld\BMP\Cursor.bmp");
