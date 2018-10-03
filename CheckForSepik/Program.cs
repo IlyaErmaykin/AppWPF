5e561054c1e8e1d2cc4e29a456f6fa82348fee3c
Ермайкин Илья <ilya_ermaykin@mail.ru>
2018-10-03 10:19:09 +0400

    #9 Добавлен проект CheckForSepik;  - Первое задааине;

---
+++ CheckForSepik/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckForSepik
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int value1 = int.Parse(Console.ReadLine());
+            int value2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(value1 * value2);
+        }
+    }
+}
