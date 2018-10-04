fa8f5bf74959e0641250a34429624fb4782e04b3
Ермайкин Илья <ilya_ermaykin@mail.ru>
2018-10-04 10:32:32 +0400

    #13 CheckForSepik;  - Условные операторы теоретические задания;

--- CheckForSepik/Program.cs
+++ CheckForSepik/Program.cs
@@ -10,7 +10,36 @@ namespace CheckForSepik
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("fix bugs, please");
-		}
+            int a = 10;
+            int b = 20;
+            int c = 30;
+            if (a > 5)
+            {
+                a = 5;
+                if (b < 20)
+                {
+                    a = 7;
+                }
+            }
+            b = 10;
+            if (c < b)
+            {
+                c = 100;
+            }
+            else if (c < 40)
+            {
+                c = 50;
+            }
+            else
+            {
+                c = 10;
+            }
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+
+            Console.ReadKey();
+        }
     }
 }
