4c16cef2fb839fc5118f1eeee139f90f46e92745
Ермайкин Илья <ilya_ermaykin@mail.ru>
2018-10-23 11:42:38 +0400

    #38 CheckForSepik - Методы; - Реализация возведение введенного чичла во введенную степень;

--- CheckForSepik/Program.cs
+++ CheckForSepik/Program.cs
@@ -10,61 +10,20 @@ namespace CheckForSepik
     {
         static void Main(string[] args)
         {
-            Task.CheckPairs("Console.ReadLine();", '(', ')'); /* Все в порядке */
+            int n = int.Parse(Console.ReadLine());
+            int e = int.Parse(Console.ReadLine());
 
+            Console.WriteLine(n + "^" + e + "=" + Task.Power(n, e));
             Console.ReadKey();
         }
     }
 
     public class Task
     {
-        public static void CheckPairs(string text, char openSimbol, char closeSimbol)
+        public static double Power(int number, int exponent)
         {
-            int countOpenSimbol = 0;
-            int countCloseSimbol = 0;
-            string result = "Все в порядке";
-
-            for (int i = 0; i < text.Length; i++)
-            {
-                if (text[i] == openSimbol)
-                {
-                    countOpenSimbol++;
-                    //Console.WriteLine(text[i]);
-                }
-                else if (text[i] == closeSimbol)
-                {
-                    countCloseSimbol++;
-                    //Console.WriteLine(text[i]);
-                }
-            }
-            //Console.WriteLine(text);
-
-            if (countOpenSimbol == countCloseSimbol)
-            {
-                for (int i = 0; i < countOpenSimbol; i++)
-                {
-                    int search = text.IndexOf(openSimbol);
-                    int search1 = text.IndexOf(closeSimbol);
-
-                    if (search < search1)
-                    {
-                        text = text.Remove(search, 1);
-                        text = text.Remove(search1 - 1, 1);
-                        //Console.WriteLine($"{search} {search1}");
-                        //Console.WriteLine(text);
-                    }
-                    else
-                    {
-                        result = "Найдены ошибки";
-                    }
-                }
-            }
-            else
-            {
-                result = "Найдены ошибки";
-            }
-
-            Console.WriteLine(result);
+            double result = Math.Pow(number, exponent);
+            return result;
         }
     }
 }
