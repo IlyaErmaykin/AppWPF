35ba1108c78c97680cd958c5b5cb4809df68aae8
Ермайкин Илья <ilya_ermaykin@mail.ru>
2018-11-02 09:42:25 +0400

    #41 CheckForSepik - Photoshop; - Добавлен класс Pixel; - Поле data преобразовано к типу  Pixel[*,*] - Перенос логики к новому типу данных

--- CheckForSepik/Data/Convertors.cs
+++ CheckForSepik/Data/Convertors.cs
@@ -1,3 +1,4 @@
+using CheckForSepik.Data;
 using System;
 using System.Drawing;
 
@@ -7,17 +8,19 @@ namespace MyPhotoshop
 	{
 		public static Photo Bitmap2Photo(Bitmap bmp)
 		{
-			var photo=new Photo();
-			photo.width=bmp.Width;
-			photo.height=bmp.Height;
-			photo.data=new double[bmp.Width,bmp.Height,3];
-			for (int x=0;x<bmp.Width;x++)
-				for (int y=0;y<bmp.Height;y++)
+			var photo = new Photo();
+			photo.width = bmp.Width;
+			photo.height = bmp.Height;
+			photo.data = new Pixel[bmp.Width, bmp.Height];
+			for (int x = 0; x < bmp.Width; x++)
+				for (int y = 0; y < bmp.Height; y++)
 				{
-				var pixel=bmp.GetPixel (x,y);
-				photo.data[x,y,0]=(double)pixel.R/255;
-				photo.data[x,y,1]=(double)pixel.G/255;
-				photo.data[x,y,2]=(double)pixel.B/255;
+				var pixel = bmp.GetPixel(x,y);
+
+				photo.data[x, y] = new Pixel();
+				photo.data[x,y].R = (double)pixel.R / 255;
+				photo.data[x,y].G = (double)pixel.G / 255;
+				photo.data[x,y].B = (double)pixel.B / 255;
 				}
 			return photo;
 		}
@@ -32,13 +35,12 @@ namespace MyPhotoshop
 		public static Bitmap Photo2Bitmap(Photo photo)
 		{
 			var bmp=new Bitmap(photo.width,photo.height);
-			for (int x=0;x<bmp.Width;x++)
-				for (int y=0;y<bmp.Height;y++)
+			for (int x = 0; x < bmp.Width; x++)
+				for (int y = 0; y < bmp.Height; y++)
 					bmp.SetPixel(x,y,Color.FromArgb (
-						ToChannel (photo.data[x,y,0]),
-						ToChannel (photo.data[x,y,1]),
-						ToChannel (photo.data[x,y,2]) ));
-					       		
+						ToChannel (photo.data[x,y].R),
+						ToChannel (photo.data[x,y].G),
+						ToChannel (photo.data[x,y].B) ));
 			return bmp;
 		}
 	}
