58d7aef090e0aa206f69966f37cf80a3afec30a6
Ермайкин Илья <ilya_ermaykin@mail.ru>
2019-02-05 12:04:49 +0400

    #59 - ConsoleForTest - промежуточный коммит;  - Начата реализация поиска Евклидова расстояния;

---
+++ ConsoleForTest/CountValue.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleForTest
+{
+    public class CountValue
+    {
+        /// <summary> 
+        /// Create array function 
+        /// </summary> 
+        public int[,] CreateArr()
+        {
+            int[,] MyArray = { { 1, 2 }, { 3, 4 }, { 5, 6 }, { 6, 7 }, { 9, 10 } };
+
+            //Random ran = new Random();
+
+            //// Инициализируем данный массив 
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    for (int j = 0; j < 2; j++)
+            //    {
+            //        //MyArray[i, j] = ran.Next(0, 10); 
+            //        Console.Write("{0}\t", MyArray[i, j]);
+            //    }
+            //    Console.WriteLine();
+            //}
+            return MyArray;
+        }
+
+
+        /// <summary> 
+        /// Distance calculation function,
+        /// Formula for calculating: 
+        /// </summary> D(ab) = Math.Sqrt(Math.Pow((x1 - x0), 2) + Math.Pow((y1 - y0), 2))
+        public void DistanceCalculation(int[,] newArray)
+        {
+            int[,] distanceArray = null;
+            int x0 = 0;
+            int y0 = 0;  
+            int x1 = 0;
+            int y1 = 0;
+
+            // Инициализируем данный массив 
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    //MyArray[i, j] = ran.Next(0, 10); 
+                    //Console.Write("{0}\t", newArray[i, j]);
+                    if (j < 1)
+                    {
+                        x1 = j;
+                    }
+                    else y1 = j;
+
+                    Console.WriteLine($"{x1}, {y1}");
+                }
+                //Console.WriteLine();
+            }
+        }
+
+        /// <summary> 
+        /// Sorting function 
+        /// </summary> 
+        void SortingFunction()
+        { }
+    }
+}
--- ConsoleForTest/Program.cs
+++ ConsoleForTest/Program.cs
@@ -23,49 +23,17 @@ namespace ConsoleForTest
     class Program
     {
         /// <summary> 
-        /// Create array function 
-        /// </summary> 
-        static int[,] CreateArr()
-        {
-            int[,] MyArray = { { 1, 2 }, { 3, 4 }, { 5, 6 }, { 6, 7 }, { 9, 10 } };
-
-            //Random ran = new Random();
-
-            // Инициализируем данный массив 
-            for (int i = 0; i < 5; i++)
-            {
-                for (int j = 0; j < 2; j++)
-                {
-                    //MyArray[i, j] = ran.Next(0, 10); 
-                    Console.Write("{0}\t", MyArray[i, j]);
-                }
-                Console.WriteLine();
-            }
-            return MyArray;
-        }
-
-
-        /// <summary> 
-        /// Distance calculation function 
-        /// </summary> 
-        void DistanceCalculation()
-        {
-
-        }
-
-        /// <summary> 
-        /// Sorting function 
-        /// </summary> 
-        void SortingFunction()
-        { }
-
-        /// <summary> 
-        /// 
+        /// Run program function
         /// </summary> 
         /// <param name="args"></param> 
         static void Main(string[] args)
         {
-            CreateArr();
+            int[,] array = null;
+
+            CountValue countValue = new CountValue();
+
+            array = countValue.CreateArr();
+            countValue.DistanceCalculation(array);
             //var arr = CreateArr
             //DistanceCalculation(int MyArray[,]);
 
