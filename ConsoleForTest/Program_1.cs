a2cc94d6a67a5182a370f5d784f40ca5c194c272
Ilya.Ermaykin <Ilya.Ermaykin@indusoft.ru>
2022-03-29 10:18:53 +0300

    #104 - ConsoleForTest - тест;

---
+++ ConsoleForTest/Program_1.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Office.Interop.Excel;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Timers;
+using System.Threading;
+using ConsoleForTest;
+using System.Windows;
+
+namespace ConsoleForTest
+{
+   
+    class Program_1
+    {
+        static void Main(string[] args)
+        {
+            //тест
+        }
+    }
+}
