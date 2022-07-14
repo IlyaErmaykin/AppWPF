168a6e5e4ca59feb3def4be71a33b3015a5b56bf
Ilya.Ermaykin <Ilya.Ermaykin@indusoft.ru>
2022-07-14 18:11:43 +0300

    #107 -  AppForDesignPatterns;  - Разбор паттерна "Фабричный метод";

---
+++ AppForDesignPatterns/Patterns/CreationalPatterns/FactoryMethod/FactoryMethod.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppForDesignPatterns.Patterns.FactoryMethod
+{
+    class FactoryMethod
+    {
+    }
+
+    abstract class Product
+    { }
+
+    class ConcreteProductA : Product
+    { }
+
+    class ConcreteProductB : Product
+    { }
+
+    abstract class Creator
+    { 
+        public abstract Product FactoryMethod();
+    }
+
+    class ConcreteCreatorA : Creator 
+    {
+        public override Product FactoryMethod() { return new ConcreteProductA(); }
+    }
+
+    class ConcreteCreatorB : Creator
+    {
+        public override Product FactoryMethod() { return new ConcreteProductB(); }
+    }
+}
